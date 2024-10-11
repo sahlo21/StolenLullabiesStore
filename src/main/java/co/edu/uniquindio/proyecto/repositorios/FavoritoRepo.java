2d7ec4a88ad2e07686f5f4b1ae83d98bb3af9d3b
sahlo21 <kssm21p@gmail.com>
2024-10-10 20:50:42 -0500

    Repositorios v1

---
+++ src/main/java/co/edu/uniquindio/proyecto/repositorios/FavoritoRepo.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.proyecto.repositorios;
+
+import co.edu.uniquindio.proyecto.modelo.entidades.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoritoRepo extends JpaRepository<Administrador, Integer> {
+}
